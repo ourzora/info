9841e54b1770646306b8ce0c424583ac73a1f3eb
Rohan Kulkarni <kulkarohan@gmail.com>
2024-03-06 23:04:41 -0500

    feat: use zora network subgraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,39 +4,42 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev',
+    // uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev',
+    uri: 'https://api.goldsky.com/api/public/project_clhk16b61ay9t49vm6ntn4mkz/subgraphs/uniswap-v2-zora-network/1.0.0/gn'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
 })
 
-export const healthClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/index-node/graphql',
-  }),
-  cache: new InMemoryCache(),
-  shouldBatch: true,
-})
+// TODO
 
-export const v1Client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
-  }),
-  cache: new InMemoryCache(),
-  shouldBatch: true,
-})
+// export const healthClient = new ApolloClient({
+//   link: new HttpLink({
+//     uri: 'https://api.thegraph.com/index-node/graphql',
+//   }),
+//   cache: new InMemoryCache(),
+//   shouldBatch: true,
+// })
 
-export const stakingClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
-  }),
-  cache: new InMemoryCache(),
-  shouldBatch: true,
-})
+// export const v1Client = new ApolloClient({
+//   link: new HttpLink({
+//     uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
+//   }),
+//   cache: new InMemoryCache(),
+//   shouldBatch: true,
+// })
 
-export const blockClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
-  }),
-  cache: new InMemoryCache(),
-})
+// export const stakingClient = new ApolloClient({
+//   link: new HttpLink({
+//     uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
+//   }),
+//   cache: new InMemoryCache(),
+//   shouldBatch: true,
+// })
+
+// export const blockClient = new ApolloClient({
+//   link: new HttpLink({
+//     uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+//   }),
+//   cache: new InMemoryCache(),
+// })
