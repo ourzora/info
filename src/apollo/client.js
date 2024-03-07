42b98116f58fe8451acff8215da17eeadc8d2d98
Rohan Kulkarni <kulkarohan@gmail.com>
2024-03-07 14:09:01 -0500

    chore: update client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -17,28 +17,26 @@ export const blockClient = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-// TODO
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: '',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
 
-// export const healthClient = new ApolloClient({
-//   link: new HttpLink({
-//     uri: 'https://api.thegraph.com/index-node/graphql',
-//   }),
-//   cache: new InMemoryCache(),
-//   shouldBatch: true,
-// })
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: '',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
 
-// export const v1Client = new ApolloClient({
-//   link: new HttpLink({
-//     uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
-//   }),
-//   cache: new InMemoryCache(),
-//   shouldBatch: true,
-// })
-
-// export const stakingClient = new ApolloClient({
-//   link: new HttpLink({
-//     uri: 'https://api.thegraph.com/subgraphs/name/way2rach/talisman',
-//   }),
-//   cache: new InMemoryCache(),
-//   shouldBatch: true,
-// })
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: '',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
